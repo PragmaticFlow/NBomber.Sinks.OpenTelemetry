b997259da8fec24798ab929558c9005b0979d4c3
Oleksandr <oleksandr.m.dev@gmail.com>
2025-10-31 14:30:19 +0200

    Add OpenTelemetry reporting sink

---
+++ src/NBomber.Sinks.OpenTelemetry/CustomMetricsReader.cs
@@ -0,0 +1,10 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Metrics;
+
+namespace NBomber.Sinks.OpenTelemetry
+{
+    class CustomMetricsReader : BaseExportingMetricReader
+    {
+        public CustomMetricsReader(BaseExporter<Metric> exporter) : base(exporter) { }
+    }
+}
